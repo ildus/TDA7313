2265dd3bf8b72da235aed3a77c2e08e498bb437b
Ildus K <i.kurbangaliev@gmail.com>
2017-01-16 00:38:10 +0500

    Add base functions

--- TDA7313.cpp
+++ TDA7313.cpp
@@ -1,22 +1,66 @@
-#include <Wire.h>
 #include "TDA7313.h"
+#include <cassert>
 
 TDA7313::TDA7313() {
+#ifdef Arduino_h
 	Wire.begin();
+#endif
 
 	//initizalize volume, set to -78.75dB
 	vol_ctrl_data.high_bits = 0;
-	vol_ctrl_data.b = 0b111;
+	vol_ctrl_data.b = 0b111; //lowest level
 	vol_ctrl_data.a = 0b111;
+
+	//initialize audio switch
+	switch_data.high_bits = 0b010;
+	switch_data.gain = 0; // 11.25dB
+	switch_data.loudness = 0; // loudness on
+	switch_data.input = 0; // input 1
+
+	//initialize bass
+	bass_data.high_bits = 0b0110;
+	bass_data.level = 0; // -14dB
+
+	//initialize treble
+	treble_data.high_bits = 0b0111;
+	treble_data.level = 0; // -14dB
+
+	//initialize attenuators
+	lf_att_data.high_bits = 0b100;
+	lf_att_data.b = 0;
+	lf_att_data.a = 0;
+
+	rf_att_data.high_bits = 0b101;
+	rf_att_data.b = 0;
+	rf_att_data.a = 0;
+
+	lr_att_data.high_bits = 0b110;
+	lr_att_data.b = 0;
+	lr_att_data.a = 0;
+
+	rr_att_data.high_bits = 0b111;
+	rr_att_data.b = 0;
+	rr_att_data.a = 0;
 }
 
 /* I2C communication */
 void TDA7313::write_data(unsigned char *buf, int length) {
+#ifdef Arduino_h
 	Wire.beginTransmission(0x44); // 01000100
 	for (int i = 0; i < length; i++) {
 		Wire.write(buf[i]);
 	}
 	Wire.endTransmission();
+#endif
+}
+
+void TDA7313::set_volume(unsigned char vol) {
+	vol_ctrl_data.b = vol >> 3;
+	vol_ctrl_data.a = vol & 0b00000111;
+}
+
+unsigned char TDA7313::get_volume(void) {
+	return (vol_ctrl_data.b << 3) + vol_ctrl_data.a;
 }
 
 /*
@@ -46,8 +90,106 @@ void TDA7313::decrease_volume(void) {
 }
 
 void TDA7313::apply(void) {
-	unsigned char buf[] = {0};
+	unsigned char buf[] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 	memcpy(buf, &vol_ctrl_data, 1);
-	this->write_data(buf, 1);
+	memcpy(buf + 1, &switch_data, 1);
+	memcpy(buf + 2, &bass_data, 1);
+	memcpy(buf + 3, &treble_data, 1);
+
+	//attenuators
+	for (int i; i < 4; i++) {
+		attenuator_ctrl *att_data = get_attenuator(i);
+		assert(att_data != NULL);
+		memcpy(buf + 4 + i, att_data, 1);
+	}
+	this->write_data(buf, sizeof(buf));
+}
+
+void TDA7313::set_input(int num) {
+	assert(num >= 0 && num <= 2);
+	switch_data.input = num;
+}
+
+void TDA7313::set_loudness(bool on) {
+	switch_data.loudness = int(!on);
+}
+
+/*
+ * Set gain
+ * 0	11.25dB
+ * 1	7.5dB
+ * 2	3.75dB
+ * 3	0dB
+ */
+void TDA7313::set_gain(int num) {
+	assert(num >= 0 && num <= 3);
+	switch_data.gain = num;
+}
+
+void TDA7313::mute(void) {
+	for (int i; i < 4; i++) {
+		attenuator_ctrl *att_data = get_attenuator(i);
+		assert(att_data != NULL);
+		att_data->b = 0b11;
+		att_data->a = 0b111;
+	}
+}
+
+attenuator_ctrl *TDA7313::get_attenuator(int input) {
+	switch (input) {
+		case 0: return &lf_att_data;
+		case 1: return &rf_att_data;
+		case 2: return &lr_att_data;
+		case 3: return &rr_att_data;
+	}
+	return NULL;
+}
+
+void TDA7313::attenuator_set_value(int input, unsigned char value) {
+	attenuator_ctrl *att_data = get_attenuator(input);
+	if (att_data == NULL)
+		return;
+
+	value &= 0b00011111;
+	att_data->b = value >> 3;
+	att_data->a = value & 0b00111;
+}
+
+unsigned char TDA7313::attenuator_get_value(int input) {
+	attenuator_ctrl *att_data = get_attenuator(input);
+	assert(att_data != NULL);
+	return (att_data->b << 3) + att_data->a;
+}
+
+/* minimum: -38.75dB, means it's muted */
+void TDA7313::attenuator_decrease(int input) {
+	attenuator_ctrl *att_data = get_attenuator(input);
+	if (att_data == NULL)
+		return;
+
+	if ((att_data->b == 0b11) && (att_data->a == 0b111))
+		/* nothing to decrease */
+		return;
+
+	att_data->a += 1;
+	if (att_data->a == 0) { // overflow
+		att_data->b += 1;
+	}
+}
+
+/* maximum: 0 */
+void TDA7313::attenuator_increase(int input) {
+	attenuator_ctrl *att_data = get_attenuator(input);
+	if (att_data == NULL)
+		return;
+
+	if ((att_data->b == 0) && (att_data->a == 0))
+		/* nothing to increase */
+		return;
+
+	att_data->a -= 1;
+	if (att_data->a == 0b111) { // overflow
+		att_data->b -= 1;
+	}
 }
--- TDA7313.h
+++ TDA7313.h
@@ -1,7 +1,9 @@
 #ifndef _TDA7313_H_
 #define _TDA7313_H_
 
+#ifdef Arduino_h
 #include "Wire.h"
+#endif
 
 /* Volume:
  * -78.75dB ... 0
@@ -14,18 +16,64 @@ struct volume_ctrl
 	unsigned int a:3;
 };
 
-const double	MAX_VOLUME = 0,
-				MIN_VOLUME = -78.75;
+struct audio_switch_ctrl
+{
+	unsigned int high_bits:3;
+	unsigned int gain:2;
+	unsigned int loudness:1;
+	unsigned int input:2;
+};
+
+struct bass_and_treble_ctrl
+{
+	unsigned int high_bits:4;
+	unsigned int level:4;
+};
+
+struct attenuator_ctrl
+{
+	unsigned int high_bits: 3;
+	unsigned int b: 2;
+	unsigned int a: 3;
+};
 
 class TDA7313 {
 	private:
 		volume_ctrl vol_ctrl_data;
+		audio_switch_ctrl switch_data;
+		bass_and_treble_ctrl bass_data;
+		bass_and_treble_ctrl treble_data;
+		attenuator_ctrl lf_att_data;
+		attenuator_ctrl rf_att_data;
+		attenuator_ctrl lr_att_data;
+		attenuator_ctrl rr_att_data;
+
 		void write_data(unsigned char *buf, int length);
+		attenuator_ctrl* get_attenuator(int input);
 	public:
 		TDA7313();
-		void set_input(int number);
+
+		/* select one of three inputs, starting from 0 */
+		void set_input(int num);
+
+		/* audio switch functions */
+		void set_loudness(bool on);
+		void set_gain(int num);
+
+		/* volume related functions */
+		unsigned char get_volume(void);
+		void set_volume(unsigned char vol);
 		void increase_volume(void);
 		void decrease_volume(void);
+		void mute(void);
+
+		/* attenuators */
+		void attenuator_set_value(int input, unsigned char value);
+		unsigned char attenuator_get_value(int input);
+		void attenuator_decrease(int input);
+		void attenuator_increase(int input);
+
+		/* apply all changes */
 		void apply(void);
 };
 
